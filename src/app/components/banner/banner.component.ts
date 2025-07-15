d7f45a76973ee5eb710d6ccdb0518753408497c6
--global <selgeandreas02@gmail.com>
2025-07-15 14:38:02 +0300

    add mock data for banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  bannerData: any;
 
+  ngOnInit(): void {
+    this.bannerData = {
+      title: 'Pulss',
+      description: 'Raske hingata 1/4',
+      lead: `<p>Ilmari saab teada Meeri tagasitulekust ja mõistab, et tõe rääkimist ei ole võimalik kauem edasi lükata. Jesse korraldab Sonja lastele üllatuse. Marjut unustab Mila külaskäigu, kuid üritab vapralt head nägu teha.</p>`,
+      imageUrl: 'https://s.err.ee/photo/crop/2025/07/14/2941596h7f09.jpg',
+      link: 'https://jupiter.err.ee/1609705010'
+    };
+  }
 }
