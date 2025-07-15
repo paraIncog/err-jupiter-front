20b55767a8d33b5658a8c45f7d956fef7e271840
--global <selgeandreas02@gmail.com>
2025-07-15 15:24:14 +0300

    real-time banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { ContentRowComponent } from './components/content-row/content-row.component';
 import { ContentService } from './services/content.service';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +15,22 @@ import { CommonModule } from '@angular/common';
 })
 
 export class AppComponent implements OnInit {
-  title = 'err-jupiter-front';
   frontPageRows: any[] = [];
+  bannerItem: any[] = [];
 
-  constructor(private contentService: ContentService) { }
+  title = 'err-jupiter-front';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.contentService.getFrontPageContent().subscribe((response: any[]) => {
-      this.frontPageRows = response.map(topic => ({
-        title: topic.header,
-        data: topic.data
-      }))
+    this.http.get('https://services.err.ee/api/v2/category/getByUrl?url=video&domain=jupiter.err.ee').subscribe((response: any) => {
+      const frontPage = response?.data?.category?.frontPage || [];
+
+      const bannerBlock = frontPage.find((block: any) => block.manual?.banner && block.data?.length > 0);
+      this.bannerItem = bannerBlock?.data[0];
+
+      this.frontPageRows = frontPage.filter((block: any) => !block.manual?.banner);
+      this.frontPageRows = frontPage.filter((block: any) => !block.manual?.banner);
     });
   }
 }
